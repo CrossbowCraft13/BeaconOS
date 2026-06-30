297fd8e7de1efeecf0d40d47e9053427571b3e09
Hunter <154709300+CrossbowCraft13@users.noreply.github.com>
2026-06-29 22:38:49 -0500

    Simplify command handling in index.ts
    Removed version and help commands from the index file.

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,18 @@
 #!/usr/bin/env node
 
 import { createServer } from "./commands/create";
-import { showVersion } from "./commands/version";
-import { showHelp } from "./commands/help";
 
 const args = process.argv.slice(2);
 
 const command = args[0];
 
 switch (command) {
+  case "create":
+    createServer(args[1]);
+    break;
 
-    case "create":
-        createServer(args[1]);
-        break;
-
-    case "version":
-        showVersion();
-        break;
-
-    case "help":
-        showHelp();
-        break;
-
-    default:
-        showHelp();
-}
+  default:
+    console.log(`
 BeaconOS v0.2 Alpha
 
 Commands:
