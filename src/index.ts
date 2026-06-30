8032928fadb0cc74ec0e8bf70a2204546668fa0b
Hunter <154709300+CrossbowCraft13@users.noreply.github.com>
2026-06-29 22:07:30 -0500

    Implement version and help commands in CLI
    Added version and help commands to the CLI.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import { createServer } from "./commands/create";
+import { showVersion } from "./commands/version";
+import { showHelp } from "./commands/help";
 
 const args = process.argv.slice(2);
 
@@ -8,12 +10,21 @@ const command = args[0];
 
 switch (command) {
 
-  case "create":
-    createServer(args[1]);
-    break;
+    case "create":
+        createServer(args[1]);
+        break;
 
-  default:
-    console.log(`
+    case "version":
+        showVersion();
+        break;
+
+    case "help":
+        showHelp();
+        break;
+
+    default:
+        showHelp();
+}
 BeaconOS v0.2 Alpha
 
 Commands:
